152f2f1354c1fb7ed853d6a3d6ba00ec4e5aeda7
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2024-06-14 14:34:55 +0200

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="bg-green-500">
+      <img src="https://via.placeholder.com/150" alt="logo" />
+    </header>
+  );
+}
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div className="bg-blue-500">
+      <h1 className="text-3xl">Main</h1>
+    </div>
+  );
+}
